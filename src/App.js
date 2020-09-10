1508596527f50b7b41093ccf3e4a55b1ccd73b9c
Zelcus <zacharias.eryd.berlin@lime.tech>
2020-09-10 16:25:28 +0200

    Convert css to sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import "./App.scss";
 
 import DaysToSpooktober from "./daysToSpooktober";
 import ScoreBoard from "./scoreBoard";
